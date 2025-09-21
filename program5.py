5bc5df10356fefc612e88e2fd2758663e9b2f71f
sonujayaprakash <sonuprakash173@gmail.com>
2025-09-22 00:50:16 +0530

    edit

--- program5.py
+++ program5.py
@@ -1,3 +1,8 @@
 num = int(input("Enter a number:"))
 
 print("Square of the number is:", num*num)
+
+# there are 2 more ways to do this
+print("Square is:",num**num) # this is used to find the cube of the number
+
+print("Square is:", num^2) # this is an incorrect way of doing this problem
