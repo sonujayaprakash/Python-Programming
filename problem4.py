1d49c51b8325f008f7988b61784fc7755f3feb6d
sonujayaprakash <sonuprakash173@gmail.com>
2025-09-22 00:51:11 +0530

    edi

--- problem4.py
+++ problem4.py
@@ -4,6 +4,6 @@ b = int(input("Enter another number: "))
 sum = a + b
 avg = sum/2
 
-print("Average of two numbers are :", avg)
+print("Average of two numbers are :", avg) # elaborative method and takesup more time
 
-print("average:", (a+b)/2)
+print("average:", (a+b)/2) # simple way to do this
