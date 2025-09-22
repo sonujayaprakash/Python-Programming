b1e30e1b151599f344c8f54d9b0b6ba912028340
sonujayaprakash <sonuprakash173@gmail.com>
2025-09-23 00:08:46 +0530

    problems

--- escape_sequence.py
+++ escape_sequence.py
@@ -1,2 +1,3 @@
-a = "I have gone for a date with a guy,\nand his name is Sidharth"
+a = "I have gone for a date with a guy,\nand his name is\t \"Sidharth\""
 print(a)
+#\n is used to print on a new line and \t is used to print some space, \" is used to print double quotes in code
---
+++ problem6.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+
+# print("Good Afternoon",name) this is an old method
+
+print(f"Good Afternoon {name}!") #f is needed for this type of code and is the newest method 
