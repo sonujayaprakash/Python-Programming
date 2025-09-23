a6c59f52194f609c4505d7824fc1e9ffb1bfa8d3
sonujayaprakash <sonuprakash173@gmail.com>
2025-09-23 11:05:09 +0530

    lists

---
+++ lists.py
@@ -0,0 +1,5 @@
+a = ["Salih","Alappi","Rashap","Jaasu","Amir","Alee","Sonu",1,5.3,True]
+# the list is mutable and can contain any datatypes
+print(2)
+a[1] = "Alan"
+print(a)
--- problem8.py
+++ problem8.py
@@ -1,3 +1,9 @@
 a ="Hello  World!"
 
 print(a.find("  "))
+
+print(a.replace("  "," "))
+
+b = "Dear Sonu,\nthis python course is nice. \nThanks!"
+
+print(b)
