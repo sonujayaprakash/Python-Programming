888eadc5fed4f3ded68bc12b6a1118b24a7378fa
sonujayaprakash <sonuprakash173@gmail.com>
2025-09-24 00:25:36 +0530

    problems

---
+++ problem11.py
@@ -0,0 +1,13 @@
+nums = []
+
+n1 = int(input("Enter a number: "))
+nums.append(n1)
+n2 = int(input("Enter a number: "))
+nums.append(n2)
+n3 = int(input("Enter a number: "))
+nums.append(n3)
+n4 = int(input("Enter a number: "))
+nums.append(n4)
+
+numbers = sum(nums)
+print(numbers)
