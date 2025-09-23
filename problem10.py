480b13557a9251a1c1a867c5065d3f034c2cd4bc
sonujayaprakash <sonuprakash173@gmail.com>
2025-09-23 23:36:20 +0530

    problems

---
+++ problem10.py
@@ -0,0 +1,18 @@
+marks = []
+
+m1 = int(input("Enter your marks"))
+marks.append(m1)
+m2 = int(input("Enter your marks"))
+marks.append(m2)
+m3 = int(input("Enter your marks"))
+marks.append(m3)
+m4 = int(input("Enter your marks"))
+marks.append(m4)
+m5 = int(input("Enter your marks"))
+marks.append(m5)
+m6 = int(input("Enter your marks"))
+marks.append(m6)
+print(marks)
+
+marks.sort()
+print(marks)
