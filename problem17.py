de1a336f6d815a3ebe42f4e171ed9199f1f6513a
sonujayaprakash <sonuprakash173@gmail.com>
2025-10-05 14:01:56 +0530

    problems

---
+++ problem17.py
@@ -0,0 +1,8 @@
+Students = ["Deena","Sidharth","Sonu","Arjun","Aleena","Jasmine","Sana"]
+
+name = input("Enter a name: ")
+
+if(name in Students) :
+    print("Student")
+else :
+    print("Not a student")
